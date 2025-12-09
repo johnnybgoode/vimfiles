f3c73b9ba75d2a6e783b3a5bb41140b072f0a346
John Entwistle <j.entwistle76@gmail.com>
2025-12-08 21:34:26 -0500

    Install vim-plug

--- move-in.sh
+++ move-in.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-for file in $(ls . | grep -v "move-in"); do
+ROOT_DIR="$(cd "$(dirname "$0")" &> /dev/null && pwd -P)"
+
+# Install vim-plug
+curl --create-dirs -o "$ROOT_DIR/vim/autoload/plug.vim" \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+exit 0
+for file in $(ls "$ROOT_DIR" | grep -v "move-in"); do
   echo "Linking '$file'..."
-  ln -ns "$(pwd)/$file" "$HOME/.$file" 
+  ln -ns "$ROOT_DIR/$file" "$HOME/.$file"
 done
